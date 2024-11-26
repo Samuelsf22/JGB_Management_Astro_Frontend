e587623646bffcd6620cd407ab65960d4eda965e
SamuelSF22 <138950240+Samuelsf22@users.noreply.github.com.>
2024-11-25 21:57:47 -0500

    add user service and type definitions for fetching user data

---
+++ src/components/types/api.ts
@@ -0,0 +1,15 @@
+export type User = {
+    id:               number;
+    password:         string;
+    last_login:       Date | null;
+    is_superuser:     boolean;
+    username:         string;
+    first_name:       string;
+    last_name:        string;
+    email:            string;
+    is_staff:         boolean;
+    is_active:        boolean;
+    date_joined:      Date;
+    groups:           number[];
+    user_permissions: number[];
+}
