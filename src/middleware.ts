a3d0a056241d079a5405b115eeff696b40eeb25b
SamuelSF22 <138950240+Samuelsf22@users.noreply.github.com.>
2024-12-01 22:17:41 -0500

    feat: enhance authentication middleware to support role-based access and improve login flow

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,29 @@
 import { defineMiddleware } from "astro:middleware";
 import { getSession } from "auth-astro/server";
 
-const LOGIN_PATHS = ["/login", "/api"];
-
-const AUTHORIZED_PATHS = ["/"];
+const AUTH_PATHS = "/api";
+const LOGIN_PATHS = ["/login"];
+const ROLE_TEACHER_PATHS = ["/instructor"];
+const ROLE_MANAGEMENT_PATHS = ["/management", "/"];
 
 export const onRequest = defineMiddleware(async (context, next) => {
   if (context.url.pathname.startsWith("/_image")) {
+    return next();
+  }
+  
+  if (context.url.pathname.startsWith(AUTH_PATHS)) {
     return next();
   }
 
   const isLogInPath = LOGIN_PATHS.some((prefix) =>
     context.url.pathname.startsWith(prefix)
   );
-  const isPrivatePath = AUTHORIZED_PATHS.some((prefix) =>
-    context.url.pathname.startsWith(prefix)
-  );
 
   const session = await getSession(context.request);
-  let isLogIn = false;
 
   if (session) {
-    isLogIn = await logIn(session.user!.email as string);
-    if (!isLogIn) {
-      
+    const { success, role } = await logIn(session.user!.email as string);
+    if (!success) {
       context.cookies.delete("__Host-authjs.csrf-token", {
         path: "/",
         secure: true,
@@ -37,9 +37,9 @@ export const onRequest = defineMiddleware(async (context, next) => {
         secure: true,
       });
 
-      context.cookies.delete("authjs.csrf-token")
-      context.cookies.delete("authjs.callback-url")
-      context.cookies.delete("authjs.session-token")
+      context.cookies.delete("authjs.csrf-token");
+      context.cookies.delete("authjs.callback-url");
+      context.cookies.delete("authjs.session-token");
 
       context.cookies.set("auth", "true", {
         domain: import.meta.env.DOMAIN,
@@ -53,13 +53,33 @@ export const onRequest = defineMiddleware(async (context, next) => {
       });
       return context.redirect("/login", 302);
     }
+    if (success) {
+      const isTeacher = role.includes("Teacher");
+      const isManagement = role.includes("Management");
 
-    if (session && isLogIn) {
       if (context.url.pathname === "/login") {
-        return context.redirect("/", 302);
+        return context.redirect(isTeacher ? "/instructor" : "/management", 302);
       }
-      if (isPrivatePath) {
+
+      const currentPath = context.url.pathname;
+      const isTeacherPath = ROLE_TEACHER_PATHS.some((path) =>
+        currentPath.startsWith(path)
+      );
+      const isManagementPath = ROLE_MANAGEMENT_PATHS.some((path) =>
+        currentPath.startsWith(path)
+      );
+
+      if (isTeacher && isTeacherPath) {
         return next();
+      }
+      if (isManagement && isManagementPath) {
+        return next();
+      }
+      if (isManagement && isTeacherPath) {
+        return context.redirect("/management", 302);
+      }
+      if (isTeacher && isManagementPath) {
+        return context.redirect("/instructor", 302);
       }
     }
   }
@@ -83,11 +103,11 @@ const logIn = async (email: string) => {
     const data = await response.json();
     if (data.message === "Successful login") {
       console.log("Successful login");
-      return true;
+      return { success: true, role: data.user.groups };
     }
     console.log("Failed login");
-    return false;
+    return { success: false };
   } catch (error) {
-    return false;
+    return { success: false };
   }
 };
