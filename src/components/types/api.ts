cda30b35ad1cb2f36e21da05517d367cd3deb3c8
SamuelSF22 <138950240+Samuelsf22@users.noreply.github.com.>
2024-12-02 23:16:42 -0500

    feat: implement course enrollment display and improve course fetching logic

--- src/components/types/api.ts
+++ src/components/types/api.ts
@@ -16,8 +16,7 @@ export type Teacher = {
 export type Subject = {
     id_subject: number;
     name:       string;
-    course:     Course;
-    teacher:    TeacherClass;
+    course:     CourseClass;
 }
 
 export type TeacherClass = {
@@ -29,6 +28,7 @@ export type TeacherClass = {
 export type User = {
     id:               number;
     groups:           string[];
+    teacher:          number;
     last_login:       null;
     is_superuser:     boolean;
     first_name:       string;
@@ -41,18 +41,19 @@ export type User = {
 
 export type Course = {
     id_course:    number;
+    enrollments:  Enrollment[];
     attendances:  Attendance[];
     evaluations:  Evaluation[];
     name:         string;
     weekly_hours: number;
-    curriculum:   number;
+    curriculum:   Curriculum;
 }
 
 export type Attendance = {
     id_attendance: number;
     date:          Date;
     status:        string;
-    student:       Student;
+    student:       Attendance;
     course:        CourseClass;
 }
 
@@ -63,15 +64,41 @@ export type CourseClass = {
     curriculum:   number;
 }
 
+export type Curriculum = {
+    id_curriculum: number;
+    name:          string;
+    year:          number;
+    status:        string;
+    school_year:   number;
+}
+
+export type Enrollment = {
+    id_enrollment:   number;
+    student:         Student;
+    enrollment_date: Date;
+    status:          string;
+    curriculum:      number;
+    courses:         number[];
+}
+
 export type Student = {
     id_student:      number;
+    parent:          Parent;
     first_name:      string;
     last_name:       string;
     dni:             string;
     birth_date:      Date;
     enrollment_date: Date;
     address:         string;
-    parent:          number;
+}
+
+export type Parent = {
+    id_parent:    number;
+    dni:          string;
+    first_name:   string;
+    last_name:    string;
+    phone_number: string;
+    address:      string;
 }
 
 export type Evaluation = {
@@ -79,6 +106,5 @@ export type Evaluation = {
     type:          string;
     grade:         number;
     date:          Date;
-    student:       Student;
     course:        CourseClass;
 }
