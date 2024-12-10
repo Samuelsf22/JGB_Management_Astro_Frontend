e3ffcf169268c65f98bafcb47a595ad8be42e11e
SamuelSF22 <138950240+Samuelsf22@users.noreply.github.com.>
2024-12-10 18:47:46 -0500

    feat: enhance Table component with improved layout, search input transition, and pagination controls

--- src/components/ui/Table.tsx
+++ src/components/ui/Table.tsx
@@ -47,17 +47,14 @@ function Table<T>({ data, columns }: TableProps<T>) {
   }
 
   return (
-    <div className="p-4 rounded-xl border bg-light-background border-light-background-400 dark:bg-dark-background-900 dark:border-dark-background-500">
+    <div className="flex flex-col gap-4 p-4 rounded-xl border bg-light-background border-light-background-400 dark:bg-dark-background-900 dark:border-dark-background-500">
       <div className="flex-0">
         <div className="relative max-w-xs">
-          <label htmlFor="hs-table-export-search" className="sr-only">
-            Search
-          </label>
           <input
             type="text"
             value={filtering}
             onChange={(e) => setFiltering(e.target.value)}
-            className="py-2 px-3 ps-9 bg-transparent block w-full shadow-sm rounded-lg text-sm focus:z-10 border-light-background-400 focus:border-primary focus:ring-primary disabled:opacity-50 disabled:pointer-events-none  dark:border-dark-foreground-700 dark:focus:border-dark-background-500 dark:focus:ring-dark-foreground-500 placeholder-light-foreground-400 dark:placeholder-dark-foreground-300"
+            className="py-2 px-3 ps-9 bg-transparent block w-full shadow-sm rounded-lg text-sm focus:z-10 border-light-background-400 focus:border-primary focus:ring-primary disabled:opacity-50 disabled:pointer-events-none  dark:border-dark-foreground-700 dark:focus:border-dark-background-500 dark:focus:ring-dark-foreground-500 placeholder-light-foreground-400 dark:placeholder-dark-foreground-300 transition-all duration-300 ease-in-out"
             placeholder="Search for items"
           />
           <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none ps-3">
@@ -68,96 +65,109 @@ function Table<T>({ data, columns }: TableProps<T>) {
           </div>
         </div>
       </div>
-      <table
-        className="divide-y divide-light-background-400 dark:divide-dark-background-500"
-        width={table.getTotalSize()}
-      >
-        <thead>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th
-                  key={header.id}
-                  className="py-3 text-xs font-bold uppercase relative px-2 hover:bg-light-background-100 dark:hover:bg-dark-background-800"
-                  onClick={header.column.getToggleSortingHandler()}
-                >
-                  <div className="flex items-center justify-between">
-                    <span>
+      <div className="h-[575px]">
+        <table
+          className="min-w-full divide-y divide-light-background-400 dark:divide-dark-background-500"
+          width={table.getTotalSize()}
+        >
+          <thead>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th
+                    key={header.id}
+                    className="py-3 text-xs font-bold uppercase relative px-2 hover:bg-light-background-100 dark:hover:bg-dark-background-800"
+                    onClick={header.column.getToggleSortingHandler()}
+                  >
+                    <div className="flex items-center justify-between">
+                      <span>
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                      </span>
+                      <div className="flex flex-col -space-y-2">
+                        <Icon
+                          icon="mdi:chevron-up"
+                          className={`text-light-foreground-400 dark:text-dark-foreground-300 ${
+                            header.column.getIsSorted() === "desc"
+                              ? "text-primary"
+                              : ""
+                          }`}
+                          height={18}
+                        />
+                        <Icon
+                          icon="mdi:chevron-down"
+                          className={`text-light-foreground-400 dark:text-dark-foreground-300 ${
+                            header.column.getIsSorted() === "asc"
+                              ? "text-primary"
+                              : ""
+                          }`}
+                          height={18}
+                        />
+                      </div>
+                    </div>
+                    <div
+                      onMouseDown={header.getResizeHandler()}
+                      onTouchStart={header.getResizeHandler()}
+                      className={`absolute top-0 right-0 h-full w-0.5 cursor-col-resize touch-none hover:bg-light-background-400 hover:dark:bg-dark-background-500 ${
+                        header.column.getIsResizing() ? "bg-primary" : ""
+                      }`}
+                    />
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody className="divide-y divide-light-background-400 dark:divide-dark-background-500">
+            {table.getRowModel().rows.map((row) => (
+              <tr
+                key={row.id}
+                className="group text-sm text-light-foreground-400 dark:text-dark-foreground-300 hover:bg-light-background-100 dark:hover:bg-dark-background-800 hover:text-primary dark:hover:text-dark-foreground"
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <td
+                    key={cell.id}
+                    className="px-2 py-4"
+                    width={cell.column.getSize()}
+                  >
+                    <span className="line-clamp-1">
                       {flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
+                        cell.column.columnDef.cell,
+                        cell.getContext()
                       )}
                     </span>
-                    <div className="flex flex-col -space-y-2">
-                      <Icon
-                        icon="mdi:chevron-up"
-                        className={`text-light-foreground-400 dark:text-dark-foreground-300 ${
-                          header.column.getIsSorted() === "desc"
-                            ? "text-primary"
-                            : ""
-                        }`}
-                        height={18}
-                      />
-                      <Icon
-                        icon="mdi:chevron-down"
-                        className={`text-light-foreground-400 dark:text-dark-foreground-300 ${
-                          header.column.getIsSorted() === "asc"
-                            ? "text-primary"
-                            : ""
-                        }`}
-                        height={18}
-                      />
-                    </div>
-                  </div>
-                  <div
-                    onMouseDown={header.getResizeHandler()}
-                    onTouchStart={header.getResizeHandler()}
-                    className={`absolute top-0 right-0 h-full w-0.5 cursor-col-resize touch-none hover:bg-light-background-400 dark:bg-dark-background-500 ${
-                      header.column.getIsResizing() ? "bg-primary" : ""
-                    }`}
-                  />
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody className="divide-y divide-light-background-400 dark:divide-dark-background-500">
-          {table.getRowModel().rows.map((row) => (
-            <tr
-              key={row.id}
-              className="group text-sm text-light-foreground-400 dark:text-dark-foreground-300 hover:bg-light-background-100 dark:hover:bg-dark-background-800 hover:text-primary dark:hover:text-dark-foreground"
-            >
-              {row.getVisibleCells().map((cell) => (
-                <td
-                  key={cell.id}
-                  className="px-2 py-4"
-                  width={cell.column.getSize()}
-                >
-                  <span className="line-clamp-1">
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </span>
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <div className="flex justify-between mt-2">
-        <button
-          type="button"
-          className="p-2.5 min-w-[40px] inline-flex justify-center items-center gap-x-2 text-sm rounded-full text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-          onClick={() => table.previousPage()}
-        >
-          Previous
-        </button>
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
 
-        <button
-          type="button"
-          className="p-2.5 min-w-[40px] inline-flex justify-center items-center gap-x-2 text-sm rounded-full text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-          onClick={() => table.nextPage()}
-        >
-          Next
-        </button>
+      <div className="flex items-center">
+        <div className="flex items-center justify-between space-x-2">
+          <button
+            type="button"
+            className="p-2 inline-flex justify-center items-center gap-x-2 text-sm rounded-full focus:outline-none text-light-foreground-400 dark:text-dark-foreground-300 focus:text-primary hover:text-primary dark:hover:text-dark-foreground hover:bg-light-background-100 dark:hover:bg-dark-background-800 dark:focus:text-dark-foreground disabled:opacity-50 disabled:pointer-events-none border border-transparent transition-colors duration-300 ease-in-out"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            <Icon icon="mdi:chevron-left" className="" height={22}/>
+          </button>
+
+          <button
+            type="button"
+            className="p-2 inline-flex justify-center items-center gap-x-2 text-sm rounded-full focus:outline-none text-light-foreground-400 dark:text-dark-foreground-300 focus:text-primary hover:text-primary dark:hover:text-dark-foreground hover:bg-light-background-100 dark:hover:bg-dark-background-800 dark:focus:text-dark-foreground disabled:opacity-50 disabled:pointer-events-none border border-transparent transition-colors duration-300 ease-in-out"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            <Icon icon="mdi:chevron-right" className="" height={22}/>
+          </button>
+        </div>
+        <div className="text-xs text-gray-500 ms-auto dark:text-neutral-400">
+          Page {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
+        </div>
       </div>
     </div>
   );
