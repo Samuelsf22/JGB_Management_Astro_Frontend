b5b49cd04a79d89f02bfe4d8b227d2fa0fa5774f
SamuelSF22 <138950240+Samuelsf22@users.noreply.github.com.>
2024-12-11 22:04:34 -0500

    fix: remove alias for src directory in Vite configuration

--- src/components/ui/data-table.tsx
+++ src/components/ui/data-table.tsx
@@ -18,13 +18,14 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@components/ui/table";
+} from "@/components/ui/table";
+
 import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
   DropdownMenuContent,
   DropdownMenuTrigger,
-} from "@components/ui/dropdown-menu";
+} from "@/components/ui/dropdown-menu";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
